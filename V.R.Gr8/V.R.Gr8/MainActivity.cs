8b2ebaa3d1a3d5c85b767b6252030503647c037f
naffy <nawaf.baraya@wolterskluwer.co.uk>
2017-04-26 09:23:38 +0100

    Solution and base andriod project

---
+++ V.R.Gr8/V.R.Gr8/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace V.R.Gr8
+{
+    [Activity(Label = "V.R.Gr8", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
